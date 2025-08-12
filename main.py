74f0ca49f2fe980300d8db558821cfe0cb114ee7
Niamh <niamhlongley@gmail.com>
2025-08-12 17:53:21 +0100

    Create basic GUI with tkinter

--- main.py
+++ main.py
@@ -1,58 +1,60 @@
-#from scanner_ui import ScannerApp
+from scanner_ui import ScannerApp
 from network_scan import MachineData
 
-program_on = True
-inner_menu = True
 
-while program_on:
-    print("This is a simple port scanner to use on your own machine.")
-    scan_confirm = int(input("Please choose an option [1 or 2]:\n"
-    "[1] Perform a scan\n"
-    "[2] Exit\n"))
-    if scan_confirm == 1:
-        print("Scanning.....")
-        machine = MachineData()
-        machine.initial_scan(1, 1000)
-        print("Scan Complete")
-        while inner_menu:
-            option = int(input("Please choose an option [1, 2, 3, 4, or 5]:\n"
-            "[1] View IP address and hostname\n"
-            "[2] View open and filtered ports\n"
-            "[3] View all protocols in use\n"
-            "[4] View OS detected\n"
-            "[5] Exit\n"))
+machine = MachineData()
+ui = ScannerApp(machine)
+
+
+
+# program_on = True
+# inner_menu = True
+
+# while program_on:
+#     print("This is a simple port scanner to use on your own machine.")
+#     scan_confirm = int(input("Please choose an option [1 or 2]:\n"
+#     "[1] Perform a scan\n"
+#     "[2] Exit\n"))
+#     if scan_confirm == 1:
+#         print("Scanning.....")
+#         machine = MachineData()
+#         machine.initial_scan(1, 1000)
+#         print("Scan Complete")
+#         while inner_menu:
+#             option = int(input("Please choose an option [1, 2, 3, 4, or 5]:\n"
+#             "[1] View IP address and hostname\n"
+#             "[2] View open and filtered ports\n"
+#             "[3] View all protocols in use\n"
+#             "[4] View OS detected\n"
+#             "[5] Exit\n"))
             
-            if option == 1:
-                print(f"IP address: {machine.ip_addr}\nHostname: {machine.hostname}")
-            elif option == 2:
-                for key, value in machine.open_ports.items():
-                    print(f"{key}: {value}")
-            elif option == 3:
-                for protocol in machine.protocols:
-                    print(protocol)
-            elif option == 4:
-                for os in machine.os_list:
-                    print(os)
-            elif option == 5:
-                exit_choice = input("Are you sure you want to exit? All scan data will be lost. [yes/no]\n").lower()
-                if exit_choice == "yes" or "y":
-                    inner_menu = False
-                    program_on = False
-                else:
-                    continue
-            else:
-                print("Invalid option. Enter a number between 1 and 5.\n")
-    elif scan_confirm == 2:
-        program_on = False
-    else:
-        print("Invalid option. Enter either 1 or 2.\n")
+#             if option == 1:
+#                 print(f"IP address: {machine.ip_addr}\nHostname: {machine.hostname}")
+#             elif option == 2:
+#                 for key, value in machine.open_ports.items():
+#                     print(f"{key}: {value}")
+#             elif option == 3:
+#                 for protocol in machine.protocols:
+#                     print(protocol)
+#             elif option == 4:
+#                 for os in machine.os_list:
+#                     print(os)
+#             elif option == 5:
+#                 exit_choice = input("Are you sure you want to exit? All scan data will be lost. [yes/no]\n").lower()
+#                 if exit_choice == "yes" or "y":
+#                     inner_menu = False
+#                     program_on = False
+#                 else:
+#                     continue
+#             else:
+#                 print("Invalid option. Enter a number between 1 and 5.\n")
+#     elif scan_confirm == 2:
+#         program_on = False
+#     else:
+#         print("Invalid option. Enter either 1 or 2.\n")
 
 
-#TODO: Any functionality to save the scan to a file, then load the data from that file in.
-#TODO: Add other scanning functions like protocol versions. 
-#TODO: Add error checking, e.g. for value errors. 
-#TODO: Create a tkinter GUI for the program. 
-#TODO: Allow users to enter their own IP address/hostname, e.g. enter loopback address. 
+
 
 
 
--- scanner_ui.py
+++ scanner_ui.py
@@ -1,24 +1,61 @@
-from tkinter import Tk, Button, Canvas, Text, Label
-import scan_data
+from tkinter import Tk, Button, Canvas, Text, Label, END, Entry
 import nmap
+from network_scan import MachineData
 
 class ScannerApp():
 
-    def __init__(self):
+    def __init__(self, machine_obj: MachineData):
         self.root = Tk()
         self.root.title("Network Scanner")
-        self.root.config(bg="#FFEAEA")
+        self.root.config()
 
-        self.scan_button = Button(text="Scan Your Machine", width=20)
-        self.scan_button.grid(row=0, column=0, padx=20, pady=20)
+        self.scan_button = Button(text="Scan Your Machine", width=20, command=lambda: self.scan(machine_obj)) # type: ignore
+        self.scan_button.grid(row=0, column=0, padx=20, pady=20, columnspan=2)
+
+        self.progress_label = Label()
+        self.progress_label.grid(row=1, column=0, columnspan=2)
 
         self.ip_label = Label(text="Your IP Address: ")
-        self.ip_label.grid(row=0, column=1, pady=20)
+        self.ip_label.grid(row=2, column=0, pady=20)
 
-        self.ip_text = Text(width=30, height=1)
-        self.ip_text.grid(row=0, column=2, pady=20)
+        self.ip_entry = Entry(width=30)
+        self.ip_entry.grid(row=2, column=1, pady=20, padx=20, sticky="W")
 
-        self.scan_text = Text()
-        self.scan_text.grid(row=2, column=0, columnspan=3, padx=20, pady=20)
+        self.ports = Label(text="Ports:" )
+        self.ports.grid(row=3, column=0)
+        self.ports_text = Text(height=10)
+        self.ports_text.grid(row=3, column=1, pady=20)
+
+        self.os_label = Label(text="Operating Systems Detected: ")
+        self.os_label.grid(row=4, column=0)
+        self.os_text = Text(height=10)
+        self.os_text.grid(row=4, column=1, padx=20, pady=20)
+
+        
+        #convernt state to normal before you need to change the text
+        
 
         self.root.mainloop()
+
+  
+        
+
+
+    def scan(self, machine_obj: MachineData):
+        self.progress_label.config(text="Scanning....")
+        self.progress_label.update_idletasks()
+        
+        machine_obj.initial_scan(1, 1000)
+
+        self.progress_label.config(text="Done")
+
+        self.ip_entry.insert(END, machine_obj.ip_addr)
+        self.ip_entry.config(state="readonly")
+
+        self.ports_text.insert(END, str(machine_obj.open_ports))
+        self.ports_text.config(state="disabled")
+
+        self.os_text.insert(END, str(machine_obj.os_list))
+        self.os_text.config(state="disabled")
+
+        self.scan_button.config(state="disabled")
